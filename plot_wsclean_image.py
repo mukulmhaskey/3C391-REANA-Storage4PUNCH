7ee1ba2ec4694210d632fba203555152c4e2bd2f
mukulmhaskey <79261411+mukulmhaskey@users.noreply.github.com>
2024-03-08 09:52:04 +0100

    plot_wsclean_image.py

--- plot_wsclean_image.py
+++ plot_wsclean_image.py
@@ -5,8 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 fname = '3C391_wsclean-image.fits'
-lofar=fits.open(fname)
-wcs_lofar = WCS( lofar[0].header, naxis=2 )
-ax= plt.subplot( projection=wcs_lofar )
-ax.imshow( lofar[0].data[0,0,:,:], origin='lower', vmin=0.004, vmax=0.3 )
-plt.savefig("plot_3c391.png")
+image=fits.open(fname)
+wcs_image = WCS( image[0].header, naxis=2 )
+ax= plt.subplot( projection=wcs_image )
+ax.imshow( image[0].data[0,0,:,:], origin='lower', vmin=-0.0091, vmax=0.04 )
+plt.savefig("plot_3c391_wsclean.png")
