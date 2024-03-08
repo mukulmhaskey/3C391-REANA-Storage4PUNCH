5444b093fd9e8c96a9d8de9c05ccb0326ee3398d
mukulmhaskey <79261411+mukulmhaskey@users.noreply.github.com>
2024-03-08 09:54:31 +0100

    pybdsf.py

--- pybdsf.py
+++ pybdsf.py
@@ -1,4 +1,4 @@
 import bdsf
-input_image='3c391.fits'
+input_image='3c391_casa.fits'
 img = bdsf.process_image(input_image)
-img.write_catalog(format='csv', catalog_type='srl') 	
+img.write_catalog(format='csv', catalog_type='srl')
