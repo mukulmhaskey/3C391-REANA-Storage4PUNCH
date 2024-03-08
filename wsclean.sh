ebc16436ca39e423ee5a42057602789f67c02fe6
mukulmhaskey <79261411+mukulmhaskey@users.noreply.github.com>
2024-03-08 09:49:38 +0100

    wsclean.sh

--- wsclean.sh
+++ wsclean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wsclean -multiscale -auto-mask 3 -auto-threshold 0.3 -niter 100000 -mgain 0.8 -size 2000 2000 -scale 0.33asec -name J0614+6108-spw0 J0614+6108-spw0.ms 2>&1 | tee J0614+6108-spw0-log.txt
+wsclean -multiscale -auto-mask 3 -auto-threshold 0.3 -niter 100000 -mgain 0.8 -size 480 480 -scale 2.5asec -name 3C391_wsclean 3c391_ctm_calib_spw0.ms 2>&1 | tee 3C391_wsclean.txt
