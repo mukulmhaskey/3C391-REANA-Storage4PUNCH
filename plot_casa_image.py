813f01084f0579ecaf53280990126bde046ebbcf
mukulmhaskey <79261411+mukulmhaskey@users.noreply.github.com>
2024-03-08 09:48:43 +0100

    plot_casa_image.py

--- plot_casa_image.py
+++ plot_casa_image.py
@@ -4,9 +4,9 @@ from astropy.coordinates import SkyCoord
 import numpy as np
 import matplotlib.pyplot as plt
 
-fname = '3c391.fits'
+fname = '3c391_casa.fits'
 lofar=fits.open(fname)
 wcs_lofar = WCS( lofar[0].header, naxis=2 )
 ax= plt.subplot( projection=wcs_lofar )
-ax.imshow( lofar[0].data[0,0,:,:], origin='lower', vmin=0.004, vmax=0.3 )
-plt.savefig("plot_3c391.png")
+ax.imshow( lofar[0].data[0,0,:,:], origin='lower', vmin=-0.0091, vmax=0.04 )
+plt.savefig("plot_3c391_casa.png")
