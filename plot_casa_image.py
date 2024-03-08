0993d6c09f5ee3b042e23f15ea52478d471199ed
mukulmhaskey <79261411+mukulmhaskey@users.noreply.github.com>
2024-03-08 09:53:45 +0100

    plot_casa_image.py

--- plot_casa_image.py
+++ plot_casa_image.py
@@ -5,8 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 fname = '3c391_casa.fits'
-lofar=fits.open(fname)
-wcs_lofar = WCS( lofar[0].header, naxis=2 )
-ax= plt.subplot( projection=wcs_lofar )
-ax.imshow( lofar[0].data[0,0,:,:], origin='lower', vmin=-0.0091, vmax=0.04 )
+image=fits.open(fname)
+wcs_image = WCS( image[0].header, naxis=2 )
+ax= plt.subplot( projection=wcs_image )
+ax.imshow( image[0].data[0,0,:,:], origin='lower', vmin=-0.0091, vmax=0.04 )
 plt.savefig("plot_3c391_casa.png")
